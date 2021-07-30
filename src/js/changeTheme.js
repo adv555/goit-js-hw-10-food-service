c294437f716db43a5454593c1da8dde80d757e42
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-07-30 19:37:09 +0300

    Update changeTheme.js

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -9,27 +9,27 @@ const refs = {
 };
 
 // ============ Upload Current THEME ============ //
-const uploadCurrentTheme = () => {
+const uploadCurrentTheme = e => {
   refs.switch.checked = JSON.parse(localStorage.getItem('Checked'));
   //   console.log(refs.switch.checked);
 
   if (refs.switch.checked) {
     refs.body.classList.remove(Theme.LIGHT);
     refs.body.classList.add(Theme.DARK);
+    return;
   }
-  if (!refs.switch.checked) {
-    refs.body.classList.remove(Theme.DARK);
-    refs.body.classList.add(Theme.LIGHT);
-  }
+
+  refs.body.classList.remove(Theme.DARK);
+  refs.body.classList.add(Theme.LIGHT);
 };
 
 // ============ Save Current Theme  ============ //
-const changeTheme = () => {
+const changeTheme = e => {
   localStorage.setItem('Checked', refs.switch.checked);
-  uploadCurrentTheme();
+  uploadCurrentTheme(e);
 };
 
 // ============ Listeners============ //
 
-document.addEventListener('DOMContentLoaded', uploadCurrentTheme());
+document.addEventListener('DOMContentLoaded', uploadCurrentTheme);
 refs.switch.addEventListener('change', changeTheme);
